1c10fab171ac8f949c21ca5bd46340e989a365f4
Ux3b1 <posadrc@yandex.ru>
2020-10-18 13:24:35 +0300

    finish hw1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,63 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size = 0;
 
     void clear() {
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+            size = 0;
+        }
     }
 
     void save(Resume r) {
+        if (size < storage.length - 1 && storage[size] == null) {
+            storage[size++] = r;
+        }
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = null;
+                size--;
+                remap();
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage,size);
     }
 
     int size() {
-        return 0;
+        return size;
+    }
+
+    void remap() {
+        for (int i = 0; i < storage.length; i++) {
+            for (int j = storage.length - 1; j > i; j--) {
+                if (storage[i] == null && storage[j] != null){
+                    storage[i] = storage[j];
+                    storage[j] = null;
+                }
+            }
+        }
     }
 }
