7d2b61d6df4a88f19516be1c7ba393806e8ae605
Artem Fedorov <posadrc@yandex.ru>
2020-11-10 12:49:39 +0300

    hw4 complete add reflection execute toString

--- src/com/urise/webapp/MainReflection.java
+++ src/com/urise/webapp/MainReflection.java
@@ -3,6 +3,8 @@ package com.urise.webapp;
 import com.urise.webapp.model.Resume;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class MainReflection {
     public static void main(String[] args) throws IllegalAccessException {
@@ -13,6 +15,13 @@ public class MainReflection {
         System.out.println(field.get(r));
         field.set(r,"new_uuid");
         // TODO : invoke r.toString via reflection
-        System.out.println(r);
+        try {
+            System.out.println("======");
+            Method method = r.getClass().getDeclaredMethod("toString");
+            System.out.println(method.invoke(r));
+        } catch (NoSuchMethodException | InvocationTargetException exception) {
+            exception.printStackTrace();
+        }
+//        System.out.println(r);
     }
 }
