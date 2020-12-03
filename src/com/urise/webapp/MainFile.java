7f34c4f82974eaeade2c361ac14bf83b65bd7f6e
Artem Fedorov <posadrc@yandex.ru>
2020-12-03 15:29:05 +0300

    hw7

---
+++ src/com/urise/webapp/MainFile.java
@@ -0,0 +1,14 @@
+package com.urise.webapp;
+
+import java.io.File;
+import java.io.IOException;
+
+public class MainFile {
+    public static void main(String[] args) throws IOException {
+        File file = new File("./gitignore");
+        System.out.println(file.getCanonicalPath());
+        File dir = new File("src/com/urise/webapp");
+        System.out.println(dir.getCanonicalPath());
+        System.out.println(dir.isDirectory());
+    }
+}
--- src/com/urise/webapp/TestSingleton.java
+++ src/com/urise/webapp/TestSingleton.java
@@ -1,7 +1,5 @@
 package com.urise.webapp;
 
-import com.urise.webapp.model.SectionType;
-
 public class TestSingleton {
     private static TestSingleton instance;
 
@@ -15,8 +13,6 @@ public class TestSingleton {
     }
 
     public static void main(String[] args) {
-        for (SectionType type : SectionType.values()) {
-            System.out.println(type.getTitle());
-        }
+        System.out.println();
     }
 }
