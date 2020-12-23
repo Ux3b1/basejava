45eebbdc0a28a56047f216ea679df58a2a0abf51
Ux3b1 <posadrc@yandex.ru>
2020-12-23 22:42:03 +0300

    hw15 add tomcat\servlet

--- config/populate.sql
+++ config/populate.sql
@@ -1,4 +1,4 @@
 INSERT INTO resume (uuid, full_name) VALUES
     ('7c28d7e2-048a-423d-8b54-55a60f31ae1b', 'Name1'),
     ('627268ff-fa80-4cc6-9d72-0b2cfa4c3c98', 'Name2'),
-    ('a465426c-2b8b-4382-bc3f-58f59485656b', 'Name3'),
+    ('a465426c-2b8b-4382-bc3f-58f59485656b', 'Name3');
